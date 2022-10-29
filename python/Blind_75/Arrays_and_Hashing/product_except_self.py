72b158f37bbb3106c666bf4b56618682f85ffc57
jacklmbrt07 <jacklmbrt07@gmail.com>
2022-10-28 22:51:28 -0700

    added problem

---
+++ python/Blind_75/Arrays_and_Hashing/product_except_self.py
@@ -0,0 +1,15 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res = [1] * len(nums)
+
+        left = 1
+        for i in range(len(nums)):
+            res[i] = left
+            left = left * nums[i]
+
+        right = 1
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] = res[i] * right
+            right = right * nums[i]
+
+        return res
