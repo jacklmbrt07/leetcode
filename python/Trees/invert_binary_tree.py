3e46630f6a720b0914474e1078fc905306446bd7
Jack Lambert <jacklmbrt07@gmail.com>
2022-11-03 13:30:27 -0700

    added two binary tree problems

---
+++ python/Trees/invert_binary_tree.py
@@ -0,0 +1,13 @@
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+
+        temp = root.left
+        root.left = root.right
+        root.right = temp
+
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+
+        return root
